db415b12289257dd4f947f919b9786ab8c2b1006
Ni Fang <ni.fang@NideMacBook-Pro.local>
2020-11-05 00:19:49 +0800

    [fangni] feat: implement problem2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,8 +1,15 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.IOException;
 
 @Data
 @NoArgsConstructor
@@ -10,7 +17,27 @@ import lombok.NoArgsConstructor;
 public class PersonVo {
 
   private String id;
+
+  @JsonSerialize(nullsUsing = PersonAgeSerializer.class)
   private Integer age;
+
   private String name;
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
+
+  private static class PersonAgeSerializer extends StdSerializer<Integer> {
+    protected PersonAgeSerializer() {
+      this(null);
+    }
+
+    protected PersonAgeSerializer(Class<Integer> age) {
+      super(age);
+    }
+
+    @Override
+    public void serialize(Integer age, JsonGenerator gen, SerializerProvider provider) throws IOException {
+      gen.writeNumber(0);
+    }
+  }
 }
